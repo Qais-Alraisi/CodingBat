2633ed4ef417989217e42e9d9410b5f8ee0552f8
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-03 04:11:28 +0400

    Create stringMatch.java

---
+++ src/warmup2/stringMatch.java
@@ -0,0 +1,11 @@
+public int stringMatch(String a, String b) {
+  int minLen = Math.min(a.length(),b.length());
+  int count = 0;
+  
+  for(int i = 0;i<minLen-1;i++){
+    if(a.substring(i,i+2).equals(b.substring(i,i+2))){
+      count++;
+    }
+  }
+  return count;
+}

7f39b2f85107b7691864804569b2dee9eb72ca61
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-05 01:28:24 +0400

    Create equalIsNot.java

---
+++ src/string3/equalIsNot.java
@@ -0,0 +1,14 @@
+public boolean equalIsNot(String str) {
+  int isCount = 0,notCount=0;
+    for (int i = 0; i < str.length() - 2; i++) {
+    if (str.substring(i, i + 3).equals("not")) {
+      notCount++;
+    }
+    }
+  for (int i = 0; i < str.length() - 1; i++) {
+    if (str.substring(i, i + 2).equals("is")) {
+      isCount++;
+    }
+  }
+  return isCount == notCount;
+}

aa194a53d3c706255372c3305a1f873c3f73b88f
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-05 01:59:11 +0400

    Create makeChocolate.java

---
+++ src/logic2/makeChocolate.java
@@ -0,0 +1,19 @@
+public int makeChocolate(int small, int big, int goal) {
+    int smallCount = 0;
+  int bigCount = 0;
+  for (int i = 0;goal>0;i++){
+    if(goal>=5 && bigCount<big){
+      goal = goal-5;
+      bigCount++;
+    } else if(goal>0 && smallCount<small){
+      goal=goal-1;
+      smallCount++;
+    } else{
+      break;
+    }
+  }
+  if (bigCount<=big && smallCount<=small && goal==0) return smallCount;;
+  return -1;
+}
+
+

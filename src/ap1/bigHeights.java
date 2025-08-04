31258e14403e8c9feee55694c25e6eb67f3a18ac
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-05 02:51:18 +0400

    Create bigHeights.java

---
+++ src/ap1/bigHeights.java
@@ -0,0 +1,8 @@
+public int bigHeights(int[] heights, int start, int end) {
+  int count = 0;
+  for (int i = start; i < end; i++) {
+    int diff = Math.abs(heights[i + 1] - heights[i]);
+    if (diff >= 5) count++;
+  }
+  return count;
+}

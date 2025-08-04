6677313fa8cf1c1c29e2f5df3488570c162b5231
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-05 02:54:57 +0400

    Create countX.java

---
+++ src/Recursion-1/countX.java
@@ -0,0 +1,10 @@
+int countX(String str) {
+    if (str.isEmpty()) {
+        return 0;
+    }
+    if (str.charAt(0) == 'x') {
+        return 1 + countX(str.substring(1));
+    } else {
+        return countX(str.substring(1));
+    }
+}

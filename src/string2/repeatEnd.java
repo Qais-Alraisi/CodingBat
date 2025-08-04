bb98cb8140db3bf7cd4aab912016854fbecf98ab
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-05 01:24:29 +0400

    Create repeatEnd.java

---
+++ src/string2/repeatEnd.java
@@ -0,0 +1,8 @@
+public String repeatEnd(String str, int n) {
+    String lastN = str.substring(str.length() - n);
+    String result = "";
+    for (int i = 0; i < n; i++) {
+        result += lastN;
+    }
+    return result;
+}

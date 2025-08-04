2f2dbd1bfbb587ba660a07e2de8b3830007c0b62
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-05 03:06:47 +0400

    Create groupSum6.java

---
+++ src/Recursion-2/groupSum6.java
@@ -0,0 +1,12 @@
+boolean groupSum6(int start, int[] nums, int target) {
+    if (start >= nums.length) {
+        return target == 0;
+    }
+    if (nums[start] == 6) {
+        return groupSum6(start + 1, nums, target - 6);
+    }
+    if (groupSum6(start + 1, nums, target - nums[start])) {
+        return true;
+    }
+    return groupSum6(start + 1, nums, target);
+}

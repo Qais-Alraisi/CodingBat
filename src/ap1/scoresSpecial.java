b5bec7620ff78764d56bc5a66942ad919795d7fc
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-05 02:46:12 +0400

    Create scoresSpecial.java

---
+++ src/ap1/scoresSpecial.java
@@ -0,0 +1,12 @@
+public int scoresSpecial(int[] a,int[] b){
+  return largestSpecial(a)+largestSpecial(b);
+}
+public int largestSpecial(int[] scores){
+  int max=0;
+  for(int score:scores){
+    if(score%10==0&&score>max){
+      max=score;
+    }
+  }
+  return max;
+}

0afe2013ac057fa6e4fd4121bcba09828f56f01a
Qais-Alraisi <qais.h.alraisi@gmail.com>
2025-08-05 01:26:46 +0400

    Create starOut.java

---
+++ src/string2/starOut.java
@@ -0,0 +1,12 @@
+public String starOut(String str) {
+    String result = "";
+    int len = str.length();
+    for (int i = 0; i < len; i++) {
+        if (str.charAt(i) == '*') continue;
+        if (i > 0 && str.charAt(i - 1) == '*') continue;
+        if (i < len - 1 && str.charAt(i + 1) == '*') continue;
+        result += str.charAt(i);
+    }
+    return result;
+}
+
